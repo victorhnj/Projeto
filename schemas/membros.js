99cb2e68ffb98671db14f93055a33482e6b5b752
Gabriel_Xander <gabrielantxr@gmail.com>
2023-10-04 22:03:07 -0300

    ajuste em menbros

--- schemas/membros.js
+++ schemas/membros.js
@@ -29,7 +29,7 @@ export default {
         },
         {
             name: 'destaque',
-            type: 'imageObject',
+            type: 'boolean',
             title: 'Destaque'
         }
     ]
