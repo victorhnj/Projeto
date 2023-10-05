393c81e5a224fc7d5768fd67428bcf4e4ccdacea
Gabriel_Xander <gabrielantxr@gmail.com>
2023-10-05 20:08:18 -0300

    fix satisfatcion

--- schemas/index.js
+++ schemas/index.js
@@ -8,7 +8,6 @@ import termos_regulamentacoes from "./termos_regulamentacoes"
 import resultado from "./resultado"
 import satisfacao_biopark from "./satisfacao_biopark"
 import satisfacao_curso from "./satisfacao_curso"
-import satisfacao_curso from "./satisfacao_infra"
 import satisfacao_infra from "./satisfacao_infra"
 
 export const schemaTypes = [imag_Qrcode, diretriz, objetivo, pontosPositivos, faq, membros, termos_regulamentacoes, resultado, satisfacao_biopark, satisfacao_curso, satisfacao_infra]
