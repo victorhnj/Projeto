2f9c452324c91af1e86ec150ff3606fde0de9e99
Gabriel_Xander <gabrielantxr@gmail.com>
2023-10-05 21:30:08 -0300

    alteracao em diretrizes

--- schemas/diretrizes.js
+++ schemas/diretrizes.js
@@ -8,10 +8,17 @@ export default {
         type: 'number',
         title: 'Ordem'
       },
+
+      {
+        name: 'descricao',
+        type: 'string',
+        title: 'Descricao'
+      },
+
       {
         name: 'titulo',
         type: 'string',
         title: 'Titulo'
-      },
+      }
     ]
   }
